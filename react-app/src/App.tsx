54fd265d187864a85adafae6196e6e3c6f5654a6
Kai <kmichaelmikhail@gmail.com>
2024-01-11 11:36:07 +0100

    Refactor App and ListGroup components

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,8 +1,19 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  const items = [
+    "New York",
+    "San Francisco",
+    "Vancouver",
+    "Tokyo",
+    "Singapore",
+    "Hong Kong",
+    "Sydney",
+  ];
   return (
-    <div><ListGroup /></div>
+    <div>
+      <ListGroup items={items} heading="Cities" />
+    </div>
   );
 }
 
--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,21 +1,17 @@
 import { useState } from "react";
 
-function ListGroup() {
-  const items = [
-    "New York",
-    "San Francisco",
-    "Vancouver",
-    "Tokyo",
-    "Singapore",
-    "Hong Kong",
-    "Sydney",
-  ];
+interface Props {
+  items: string[];
+  heading: string;
+}
+
+function ListGroup({ items, heading }: Props) {
   // Hook
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>There are no items in the list</p>}
       <ul className="list-group">
         {items.map((item, index) => (
