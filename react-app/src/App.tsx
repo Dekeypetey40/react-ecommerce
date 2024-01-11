9123bbdd95415276484e451beba2ea669be8b686
Kai <kmichaelmikhail@gmail.com>
2024-01-11 12:37:28 +0100

    Add Alert component and button functionality

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,24 @@
+import React, { useState } from "react";
+import Button from "./components/Button";
 import Alert from "./components/Alert";
 
 function App() {
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleClick = () => {
+    setShowAlert(true);
+  };
+
+  const handleClose = () => {
+    setShowAlert(false);
+  };
+
   return (
     <div>
-      <Alert>
-        Hello <span>World!</span>
+      <Alert show={showAlert} onClose={handleClose} color="danger">
+        Alert! Alert!
       </Alert>
+      <Button onClick={handleClick}>Show Alert</Button>
     </div>
   );
 }
--- react-app/src/components/Alert.tsx
+++ react-app/src/components/Alert.tsx
@@ -2,10 +2,38 @@ import { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
+  color:
+    | "primary"
+    | "secondary"
+    | "success"
+    | "danger"
+    | "warning"
+    | "info"
+    | "light"
+    | "dark";
+  show: boolean;
+  onClose: () => void;
 }
 
-const Alert = ({ children }: Props) => {
-  return <div className="alert alert-primary">{children}</div>;
+const Alert = ({ children, show, onClose, color = "danger" }: Props) => {
+  if (!show) {
+    return null;
+  }
+  return (
+    <div
+      className={"alert alert-dismissible fade show alert-" + color}
+      role="alert"
+    >
+      {children}
+      <button
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+        onClick={onClose}
+      ></button>
+    </div>
+  );
 };
 
 export default Alert;
